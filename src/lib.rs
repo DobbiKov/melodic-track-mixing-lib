62c3c6aff34594a960f6679d2d7c92f3279ba5e3
Yehor Korotenko <dobbikov@gmail.com>
2025-06-28 19:33:43 +0200

    removed cpp lib, writing keyanalyzis myself

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,8 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-pub mod bindings {
-    #![allow(non_upper_case_globals)]
-    #![allow(non_camel_case_types)]
-    #![allow(non_snake_case)]
-
-    // all the items produced by bindgen go **inside** this module
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
-
 pub mod audio_analyze;
 pub mod errors;
 pub mod key_detect;
 pub mod key_detect_cqt;
+pub mod key_detect_electronic;
 pub mod types;
 
 #[cfg(test)]
